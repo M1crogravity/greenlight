1f91ed40083046b4ed826c8dfc4f570591d9ba85
Naidenko Dmytro <dmytro.naidenko@ferm.in.ua>
2025-06-08 18:36:33 +0300

    add helpers add movie struct

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"maps"
 	"net/http"
 	"strconv"
 
@@ -17,4 +19,21 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+func (app *application) writeJson(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	js = append(js, '\n')
+
+	maps.Insert(w.Header(), maps.All(headers))
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
